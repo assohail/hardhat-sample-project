a3dbbfcb7e18a7bb13e6aa8e8818d0f8a3b7fe92
assohail <aamirsohail9210@gmail.com>
2024-06-20 23:25:55 +0500

    update the config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,8 +6,8 @@ module.exports = {
 
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/c24f61d37f764ef0841ed632141835d0`, // Replace with your Infura or Alchemy endpoint
-      accounts: [`0x7eaee2200a4e94f757c70babf0c85e3502b2fb7e66fc1502aba4887560361910`],
+      url: `https://sepolia.infura.io/v3/`, // Replace with your Infura or Alchemy endpoint
+      accounts: [`0x`], //account's private key
     },
   },
 };
